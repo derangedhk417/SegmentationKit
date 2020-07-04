bfae40d18f61bb69dbd4ecab6bc8551ffc9772cc
Adam Robinson <derangedhk417@gmail.com>
2020-07-04 15:42:29 -0400

    Contouring now works with zooming. There seems to be a slight skew when zooming in some cases though

--- DatasetEditor/ImageDisplay.py
+++ DatasetEditor/ImageDisplay.py
@@ -15,7 +15,8 @@ from kivy.uix.button       import Button
 from kivy.uix.floatlayout  import FloatLayout
 from kivy.core.window      import Window
 
-from kivy.utils import get_color_from_hex as hex_color
+from kivy.graphics.stencil_instructions import StencilPush, StencilUse, StencilUnUse, StencilPop
+from kivy.utils                         import get_color_from_hex as hex_color
 
 from Dataset           import Dataset
 from CustomBoxLayout   import CustomBoxLayout
@@ -24,8 +25,17 @@ from CustomFloatLayout import CustomFloatLayout
 import numpy as np
 import code
 
+# Handles the acttual sizing and drawing of the image rectangle, as well as 
+# drawing the zoom rectangle and cropping the image when zooming. The click
+# and drag events that take place to zoom to a rectangle are handled by this
+# class. The parent object (ImageDisplay), handles the setting and unsetting
+# of the is_zooming flag, which this class will read when determining how to 
+# interpret user inputs.
 class ImageManager(ButtonBehavior, CustomFloatLayout):
 	def __init__(self, *args, **kwargs):
+		# Read the padding argument from the user and adhere to it.
+		# It will interfere with normal operation of the FloatLayout constructor,
+		# so we remove the kay from kwargs before calling it.
 		if 'padding' in kwargs:
 			self.padding = kwargs['padding']
 			del kwargs['padding']
@@ -34,6 +44,8 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 
 		super(ImageManager, self).__init__(*args, **kwargs)
 
+		# Make sure the background is white. When you don't, images draw
+		# with a tint (no idea why).
 		with self.canvas:
 			Color(1, 1, 1, 1)
 			self.image_rect = Rectangle(
@@ -41,9 +53,16 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 				size=self.size
 			)
 
+		# Flag for whether or not an image is currently loaded. 
+		# Zooming events are invalid when this is false.
 		self.is_loaded = False
 
+		# These events will be used to ensure that the image draws in bounds
+		# and has the correct aspect ratio.
 		self.bind(size=self._update_dims, pos=self._update_dims)
+
+		# These events are necessary to track the clicking and dragging action
+		# that a user takes to zoom into a particular rectangle.
 		self.bind(
 			on_touch_down=self._down,
 			on_touch_up=self._up,
@@ -51,22 +70,54 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 		)
 		Window.bind(mouse_pos=self._mouse_pos)
 
+		# These keep track of where the user started clicking and dragging
+		# to zoom, the width and height of the zoom rectangle and the graphics
+		# object that draws a dashed line to indicate where the user is zooming.
 		self.click_down            = False
 		self.drag_start            = (0, 0)
 		self.drag_w                = 0
 		self.drag_h                = 0
 		self.current_zoom_rect_obj = None
 
+		# These are, respectively, the current subsection of the image
+		# data array that we are zoomed to and the last coordinates that
+		# were zoomed to, within the array. These are stored as actual
+		# indices, not float32 coordinates.
 		self.current_zoom_subarray = None
-		self.last_zoom_coordinates = None
+
+		# These are the position and dimensions of the rectangle
+		# that displays the image. They are updated every time the
+		# pos and size events fire.
+		self.display_x      = None
+		self.display_y      = None
+		self.display_width  = None
+		self.display_height = None
+
+		# These store the colors and geometry for all of the currently
+		# drawn lines. The instruction_groups list breaks each line
+		# up into an instruction group so that it can be added and 
+		# removed as a single object.
+		self.lines              = []
+		self.colors             = []
+		self.instruction_groups = []
+
+		# This is used to ensure that contours do not draw outside
+		# of the image when the user zooms in.
+		self.stencil_group = None
+
+		self.last_zoom = None
 
 
+	# Used to determine if a coordinate position lies within the specified
+	# rectangle.
 	def posInRect(self, pos, rpos, rsize):
 		if pos[0] > rpos[0] and pos[0] < rpos[0] + rsize[0]:
 			if pos[1] > rpos[1] and pos[1] < rpos[1] + rsize[1]:
 				return True
 		return False
 
+	# Fired when the mouse moves. This is used to change the cursor to a 
+	# crosshair when the parent object has is_zooming == True
 	def _mouse_pos(self, inst, val):
 
 		in_rect = self.posInRect(
@@ -82,10 +133,8 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 			Window.set_system_cursor('arrow')
 
 
+	# Initiates a zoom action and stores the coordinates where the zoom began.
 	def _down(self, inst, val):
-		view_x = val.pos[0] - self.display_x
-		view_y = val.pos[1] - self.display_y
-
 		in_rect = self.posInRect(
 			Window.mouse_pos,
 			(self.display_x, self.display_y),
@@ -96,6 +145,9 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 			self.click_down = True
 			self.drag_start = Window.mouse_pos
 
+
+	# If appropriate, executes a zoom and removes the dashed line from the
+	# canvas so that it will no longer draw.
 	def _up(self, inst, val):
 		self.click_down = False
 
@@ -135,7 +187,133 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 				self.drag_start            = (0, 0)
 				self.drag_w                = 0
 				self.drag_h                = 0
+		elif in_rect and self.parent.is_editing_contour:
+			# Calculate where the contour is in image coordinates (cartesian)
+			x, y = self.screenCoordinatesToRelativeCoordinates(val.pos[0], val.pos[1])
 
+			# Add this to the parent objects current contour (it will then)
+			# in turn, call this classes popLine method on the current contour
+			# and then this classes pushLine method to add the newly updates contour.
+			self.parent.addPointToContour(x, y)
+
+	def screenCoordinatesToRelativeCoordinates(self, x, y):
+		if self.last_zoom is not None:
+			x0_idx, x1_idx, y0_idx, y1_idx = self.last_zoom
+			xr  = ((x - self.display_x) / self.display_width) * self.img_size[0]
+			xr += x0_idx
+			xr /= self.img.shape[1]
+
+			y0_idx = self.img.shape[0] - y0_idx
+			yr  = ((y - self.display_y) / self.display_height) * self.img_size[1]
+			yr += y0_idx
+			yr /= self.img.shape[0]
+		else:
+			xr = (x - self.display_x) / self.display_width
+			yr = (y - self.display_y) / self.display_height
+
+		return xr, yr
+
+	# Given a set of coordinates from 0.0 to 1.0, scaled to the width
+	# and height of the currently loaded image, this function will 
+	# return where on the screen those coordinates lie.
+	def relativeCoordinatesToScreenCoordinates(self, x, y):
+		if self.last_zoom is not None:
+			x0_idx, x1_idx, y0_idx, y1_idx = self.last_zoom
+			xs = (x * self.img.shape[1]) - x0_idx
+			xs = (xs / self.img_size[0]) * self.display_width + self.display_x
+
+			y0_idx = self.img.shape[0] - y0_idx
+			ys = (y * self.img.shape[0]) - y0_idx
+			ys = (ys / self.img_size[1]) * self.display_height + self.display_y
+		else:
+			xs = (x * self.display_width)  + self.display_x
+			ys = (y * self.display_height) + self.display_y
+
+		return xs, ys
+
+	# Adds a new line onto the stack.
+	def pushLine(self, color, geometry):
+		isg = InstructionGroup()
+		converted_coordinates = []
+		for x, y in geometry:
+			converted_coordinates.extend(
+				self.relativeCoordinatesToScreenCoordinates(x, y)
+			)
+
+		isg.add(StencilPush())
+		isg.add(Rectangle(
+			pos=(self.display_x, self.display_y),
+			size=(self.display_width, self.display_height)
+		))
+		isg.add(StencilUse())		
+		isg.add(Color(*color))
+		isg.add(Line(
+			points=converted_coordinates,
+			width=1.0
+		))
+		isg.add(StencilUnUse())
+		isg.add(Rectangle(
+			pos=(self.display_x, self.display_y),
+			size=(self.display_width, self.display_height)
+		))
+		isg.add(StencilPop())
+
+		self.lines.append(geometry)
+		self.colors.append(color)
+		self.instruction_groups.append(isg)
+		self.canvas.add(isg)
+
+	# Pops the most recent line off of the stack.
+	def popLine(self):
+		self.canvas.remove(self.instruction_groups[-1])
+		self.instruction_groups.remove(self.instruction_groups[-1])
+		self.colors.remove(self.colors[-1])
+		self.lines.remove(self.lines[-1])
+
+	def updateContoursForZoom(self):
+		# We need to update all of the rectangles in the canvas that pertain
+		# to drawing contours.
+
+		# Get rid of the existing graphics objects.
+		for g in range(len(self.instruction_groups)):
+			self.canvas.remove(self.instruction_groups[-1])
+			self.instruction_groups.remove(self.instruction_groups[-1])
+
+		# Add new graphics instructions with appropriate coordinates.
+		
+		# Redraw them normally.
+		for geo, color in zip(self.lines, self.colors):
+			isg = InstructionGroup()
+			converted_coordinates = []
+			for x, y in geo:
+				converted_coordinates.extend([
+					self.relativeCoordinatesToScreenCoordinates(x, y)
+				])
+
+			isg.add(StencilPush())
+			isg.add(Rectangle(
+				pos=(self.display_x, self.display_y),
+				size=(self.display_width, self.display_height)
+			))
+			isg.add(StencilUse())		
+			isg.add(Color(*color))
+			isg.add(Line(
+				points=converted_coordinates,
+				width=1.0
+			))
+			isg.add(StencilUnUse())
+			isg.add(Rectangle(
+				pos=(self.display_x, self.display_y),
+				size=(self.display_width, self.display_height)
+			))
+			isg.add(StencilPop())
+			self.instruction_groups.append(isg)
+			self.canvas.add(isg)
+		
+
+
+	# Handles updating of the zoom rectangle (dashed line) and updating
+	# of the coordinates that indicate the current zoom rectangle.
 	def _move(self, inst, val):
 		if self.click_down:
 			if self.parent.is_zooming:
@@ -160,10 +338,11 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 					width=1
 				))
 				self.canvas.add(self.current_zoom_rect_obj)
-
-		view_x = val.pos[0] - self.display_x
-		view_y = val.pos[1] - self.display_y
-		
+	
+	# This ensures that the image being displayed will maintain its native 
+	# aspect ratio and will draw in bounds so that the user can see all of 
+	# it. Very importantly, display_x, display_y, display_width and display_height
+	# are set during this event.
 	def _resize(self, size, pos):
 		# We need to honor the desired padding for the image display, as 
 		# well as the aspect ratio of the loaded image.
@@ -224,9 +403,17 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 			self.display_x = pos[0] + self.padding[0]
 			self.display_y = pos[1] + self.padding[1]
 
+		self.updateContoursForZoom()
+
 	def _update_dims(self, inst, val):
 		self._resize(inst.size, inst.pos)
 
+	# Given a set of coordinates relative to the lower left corner of the 
+	# displayed image, this class will calculate appropriate indices into
+	# the underlying array that stores the image, retrieve the correct
+	# sub array and create a new texture object to display the zoomed image.
+	# This call stacks properly onto previous zoom calls, allowing the user
+	# to make multiple successive zooms.
 	def zoomTo(self, x0, x1, y0, y1):
 		# First, select the part of the image data that corresponds to the zoom
 		# rectangle.
@@ -241,7 +428,9 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 		y0_idx = int((y0 / self.display_height) * to_zoom.shape[0])
 		y1_idx = int((y1 / self.display_height) * to_zoom.shape[0])
 
-		# Account for the different coordinate system.
+		# Account for the different coordinate system. Arrays grow
+		# from top to bottom in their first dimension, while kivy screen
+		# coordinates are in the first cartesian quandrant.
 		y0_idx = to_zoom.shape[0] - y0_idx
 		y1_idx = to_zoom.shape[0] - y1_idx
 
@@ -251,10 +440,28 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 			:
 		]
 
-		self.last_zoom_coordinates = [
-			x0_idx, x1_idx, y0_idx, y1_idx
-		]
+		if self.last_zoom is not None:
+			x0_idx = (x0 / self.display_width)  * to_zoom.shape[1]
+			y0_idx = (y0 / self.display_height) * to_zoom.shape[0]
+			old_x0_idx, old_x1_idx, old_y0_idx, old_y1_idx = self.last_zoom
 
+			x0_idx += old_x0_idx
+			y0_idx  = y0_idx + (self.img.shape[0] - old_y0_idx)
+			y0_idx  = self.img.shape[0] - y0_idx
+			self.last_zoom = [
+				x0_idx, x1_idx, y0_idx, y1_idx
+			]
+		else:
+			x0_idx = (x0 / self.display_width)  * to_zoom.shape[1]
+			y0_idx = (y0 / self.display_height) * to_zoom.shape[0]
+			y0_idx = to_zoom.shape[0] - y0_idx
+			y1_idx = to_zoom.shape[0] - y1_idx
+			self.last_zoom = [
+				x0_idx, x1_idx, y0_idx, y1_idx
+			]
+
+		# Modify some variables that are necessary for the resize event to
+		# run properly.
 		self.img_size = (
 			self.current_zoom_subarray.shape[1],
 			self.current_zoom_subarray.shape[0]
@@ -264,8 +471,7 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 		self.img_buffer = np.fliplr(np.rot90(np.rot90(self.current_zoom_subarray)))
 		self.img_buffer = memoryview(self.img_buffer.flatten())
 
-		#code.interact(local=locals())
-
+		# Load a new texture object into graphics memory so it can be displayed.
 		self.image_texture = Texture.create(
 			size=self.img_size, 
 			colorfmt='bgr'
@@ -278,12 +484,15 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 
 		self.image_rect.texture = self.image_texture
 
-		# Here we force a resize.
+		# Here we force a resize. This will ensure that proper aspect ratio 
+		# is maintained.
 		self._resize(self.size, self.pos)
 
+	# Undo all zooming operations.
 	def reset(self):
 		if self.is_loaded:
 			self.current_zoom_subarray = None
+			self.last_zoom             = None
 			self.setImage(self.img)
 
 
@@ -305,10 +514,14 @@ class ImageManager(ButtonBehavior, CustomFloatLayout):
 
 		self.image_rect.texture = self.image_texture
 
-		# Here we force a resize.
+		# Here we force a resize. This will ensure that the image does not get
+		# cut off at the edges of the layout it is in.
 		self._resize(self.size, self.pos)
 
 
+# High level class that contains buttons for zooming and reseting the current
+# view of an image. Also contains the more complicated child object that
+# handles drawing, zooming and contouring of an image.
 class ImageDisplay(ButtonBehavior, CustomBoxLayout):
 	def __init__(self, *args, **kwargs):
 		super(ImageDisplay, self).__init__(*args, **kwargs)
@@ -329,16 +542,29 @@ class ImageDisplay(ButtonBehavior, CustomBoxLayout):
 			width=70
 		)
 
-
 		self.reset_button = Button(
-			text='Reset',
+			text='Reset Zoom',
 			size_hint_x=None,
 			size_hint_y=1,
-			width=70
+			width=120
 		)
+
+		self.contour_button = Button(
+			text='New Contour',
+			size_hint_x=None,
+			size_hint_y=1,
+			width=120
+		)
+
+		# This will store the geometry for any contours drawn on the image
+		# by the user. Each contour will be stored as (color, [[x0, y0], [x1, y1, ...]])
+		# The image_manager object will internally manage the graphics
+		# objects necessary to draw these.
+		self.contours = []
 
 		self.toolbar.add_widget(self.zoom_button)
 		self.toolbar.add_widget(self.reset_button)
+		self.toolbar.add_widget(self.contour_button)
 
 		self.add_widget(self.toolbar)
 
@@ -349,14 +575,60 @@ class ImageDisplay(ButtonBehavior, CustomBoxLayout):
 
 		self.add_widget(self.image_manager)
 
+		# These events just call the relevent function in this classes
+		# image_manager instance and/or set flags that it reads.
 		self.zoom_button.bind(on_press=self._zoom_pressed)
 		self.reset_button.bind(on_press=self._reset_pressed)
+		self.contour_button.bind(on_press=self._contour_pressed)
 
-		self.is_zooming = False
+		self.is_zooming         = False
+		self.is_editing_contour = False
+
+		# Tracks whether or not a contour has been added to the image
+		# manager stack for the current contour. 
+		self.contour_on_stack   = False
+
+		self.default_colors = [
+			hex_color('#FF0000'),
+			hex_color('#00FF00'),
+			hex_color('#0000FF')
+		]
 
 	def _reset_pressed(self, inst):
 		self.image_manager.reset()
 
+	def _contour_pressed(self, inst):
+		if self.is_editing_contour:
+			self.addPointToContour(*self.current_contour[0])
+
+			self.is_editing_contour  = False
+			self.contour_on_stack    = False
+			self.contour_button.text = 'New Contour'
+			self.contours.append((self.contour_color, self.current_contour))
+		else:
+			self.is_editing_contour    = True
+			self.contour_button.text   = 'Close Contour'
+			self.current_contour       = []
+			self.contour_color         = self.default_colors[
+				len(self.contours) % len(self.default_colors)
+			]
+
+	def addPointToContour(self, x, y):
+		if not self.is_editing_contour:
+			raise Exception("There is no contour being edited.")
+
+		self.current_contour.append([x, y])
+
+		if self.contour_on_stack:
+			self.image_manager.popLine()
+
+		self.image_manager.pushLine(self.contour_color, self.current_contour)
+
+		if not self.contour_on_stack:
+			self.contour_on_stack = True
+
+	# This handles toggling of the zoom state and coloring of the 
+	# button.
 	def _zoom_pressed(self, inst):
 		if self.is_zooming:
 			self.is_zooming = False
@@ -365,8 +637,6 @@ class ImageDisplay(ButtonBehavior, CustomBoxLayout):
 			self.saved_color = self.zoom_button.background_color
 			self.zoom_button.background_color = hex_color('#616161')
 			self.is_zooming = True
-
-	
 
 	def setImage(self, img):
 		self.image_manager.setImage(img)
